3613f75aa1ef5d0936c1b46eb9b5b9c9f414e301
nitish445 <n11nitishmehta@gmail.com>
2024-04-17 23:25:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Countdown from './components/countdown';
+  
+function App() {
+  return (
+    <div className="App">
+      <Countdown />
+    </div>
+  );
+}
+  
+export default App;
